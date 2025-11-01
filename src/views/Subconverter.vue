7988bba47f4d2e6daffa4c0d8bec6e2b3c33554c
俊杰 <73985488+wwpanni@users.noreply.github.com>
2025-11-02 00:15:24 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,6 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
+          "jjgo后端":"www.jjgo.top:25500/sub",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -1310,4 +1311,5 @@ export default {
   }
 };
 </script>
+
 
