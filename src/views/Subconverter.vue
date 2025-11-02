6627fa3dbbee6547d2ff352cd95a30b45bd5c2b8
俊杰 <73985488+wwpanni@users.noreply.github.com>
2025-11-02 10:38:21 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,8 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "爪云后端":"http://subapi.jjgo.top",
-          "jjgo后端":"http://www.jjgo.top",
+          "cloudflare自建后端":"https://clashapi.jjgo.top",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -349,8 +348,7 @@ export default {
           "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
-          { value: "http://subapi.jjgo.top" },
-          { value: "http://www.jjgo.top" },
+          { value: "https://clashapi.jjgo.top" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
@@ -1314,6 +1312,7 @@ export default {
   }
 };
 </script>
+
 
 
 
